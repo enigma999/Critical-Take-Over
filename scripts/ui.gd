200a2d90f8dda000091bd5c8c414d4ba658444d1
ENIGMA <stanborst@gmail.com>
2025-01-21 00:05:47 +0100

    removed unused function

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -2,10 +2,5 @@ extends CanvasLayer
 
 @onready var _bleed_bar: ProgressBar = $"top center/Bleed Bar"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func SetBleedBar(amount: float) -> void:
 	_bleed_bar.value = amount
