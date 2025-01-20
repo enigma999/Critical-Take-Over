459e7dbfd29df6344e7fdc5ca071af0aa9306145
TheRickiam <47054632+TheRickiam@users.noreply.github.com>
2025-01-20 20:41:28 +0100

    Seperated logic into functions

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -4,12 +4,12 @@ extends CharacterBody3D
 const SPEED: float = 8.0
 const ACCELERATION: float = 12.0
 const JUMP_VELOCITY: float = 4.5
-const GRAVITY: float = 10.0
+const GRAVITY_FORCE: float = 10.0
 
 #puur @export toegevoegd voor het tweaken enzo
 @export var current_speed: float = SPEED
 @export var acceleration: float = ACCELERATION
-@export var gravity:float = GRAVITY
+@export var gravity_force:float = GRAVITY_FORCE
 @export var sensitivity:float  = 0.1
 @export var minimum_angle:float = -80
 @export var maximum_angle:float = 90
@@ -18,19 +18,20 @@ const GRAVITY: float = 10.0
 
 var look_rotation: Vector2
 
-#func _ready() -> void:
-	#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
 func _physics_process(delta: float) -> void:
-		# Add the gravity.
+	gravity(delta)
+	handle_movement(delta)
+	move_and_slide()
+	set_camera_direction()
+	
+func gravity(delta: float)-> void:
 	if not is_on_floor():
-		velocity.y -= gravity * delta
-	# Handle Jump.
+		velocity.y -= gravity_force * delta
+
+func handle_movement(delta: float)-> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
-	
-	# Get the input direction and handle the movement/deceleration.
+		
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
@@ -41,10 +42,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = lerp(velocity.x, 0.0, acceleration * delta)
 		velocity.z = lerp(velocity.z, 0.0, acceleration * delta)
 
-	move_and_slide()
-	set_camera_direction()
-
-func set_camera_direction()->void:
+func set_camera_direction()-> void:
 	head.rotation_degrees.x = look_rotation.x
 	rotation_degrees.y = look_rotation.y
 
