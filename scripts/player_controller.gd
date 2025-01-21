3749e7ed836a50c4f7707815a086d655162ee2a8
TheRickiam <47054632+TheRickiam@users.noreply.github.com>
2025-01-21 23:44:35 +0100

    removed print statements

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -50,8 +50,6 @@ func gravity(delta: float)-> void:
 func handle_movement(delta: float)-> void:
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		print(can_attack)
-		print(attack_cooldown)
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
