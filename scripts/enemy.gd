38db21f4366c1a5c0716a21c3c01862a3219f3b7
TheRickiam <47054632+TheRickiam@users.noreply.github.com>
2025-01-25 22:41:12 +0100

    left this in

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -153,7 +153,6 @@ func handle_movement(delta: float)-> void:
 		input_dir = Input.get_vector("left", "right", "forward", "backward")
 		direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
-	var slide_direction: Vector3
 	if Input.is_action_just_pressed("slide") && is_on_floor() && can_slide:
 		slide_timer.start()
 		is_sliding = true
